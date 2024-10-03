3d7dc3ecfed94b6a725e24135ef330bcb4f4dfe5
Mbarca89 <mauriciobarca1989@gmail.com>
2024-10-03 09:46:30 -0300

    first commit

---
+++ src/main/java/com/mbarca/ByR/ByRApplication.java
@@ -0,0 +1,13 @@
+package com.mbarca.ByR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbarca/ByR/ByRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbarca.ByR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
